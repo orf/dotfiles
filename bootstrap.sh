06ca38d9164c3bae4cd559dc56d8e191178e81e2
Tom Forbes <tom@tomforb.es>
2019-06-21 02:30:41 +0100

    Bootstrap!

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/orf/dotfiles.git my-dotfiles-tmp
+rsync --recursive --verbose --exclude '.git' my-dotfiles-tmp/ $HOME/
+rm --recursive my-dotfiles-tmp
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew bundle -v --global
+
+#git -C "$(brew --repo homebrew/core)" fetch --unshallow
+
+if ! grep -Fxq "/usr/local/bin/fish" /etc/shells
+then
+   echo "Fish not in /etc/shells, adding"
+   echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+fi
+chsh -s /usr/local/bin/fish
+
+git lfs install --system
