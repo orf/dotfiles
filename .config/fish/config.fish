d86403f72405c702f45c6c4c91b7535ef3804dde
Tom Forbes <tom@tomforb.es>
2019-09-14 21:12:47 +0100

    Support secrets

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,3 +39,9 @@ if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
 end
+
+if test -e ~/.config/secrets.fish
+    source ~/.config/secrets.fish
+end
+
+
