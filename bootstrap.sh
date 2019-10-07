7d4b14c9045e2e165a1c1441fb9196cf1450fec2
Tom Forbes <tom@tomforb.es>
2019-10-07 01:06:22 +0100

    Install rust components

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,7 +28,10 @@ then
 fi
 
 git lfs install --system
+
 rustup-init -y
+rustup component add clippy rustfmt
+
 /usr/local/opt/fzf/install --all
 python3.7 -mpip install virtualfish
 defaultbrowser firefoxdeveloperedition
