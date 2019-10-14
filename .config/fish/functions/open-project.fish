9bf8208858f366360df27d3e63abcbddbb90a37b
Tom Forbes <tom@tomforb.es>
2019-10-14 21:01:43 +0100

    Fix broken open-project command

--- .config/fish/functions/open-project.fish
+++ .config/fish/functions/open-project.fish
@@ -1,6 +1,5 @@
 function open-project -d "Open a project"
-  set filter $argv
-  or set filter '""'
+  set filter "$argv"
   set chosen_project (git workspace list | fzf -q "$filter")
   if string length -q -- $chosen_project
      charm $GIT_WORKSPACE/$chosen_project
