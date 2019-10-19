de9a334539f090f87b502449e06823cba5458cab
Tom Forbes <tom@tomforb.es>
2019-10-19 14:43:50 +0100

    Add cargo-bloat

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,7 +56,7 @@ python3.7 -mpip install virtualfish
 
 fish -c "rustup-init -y"
 fish -c "rustup component add clippy rustfmt"
-fish -c "cargo install cargo-edit cargo-tree"
+fish -c "cargo install cargo-edit cargo-tree cargo-bloat"
 /usr/local/opt/fzf/install --all
 defaultbrowser firefoxdeveloperedition
 fish -c "fisher"
