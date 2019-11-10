e4e4a84021c74b1ed9f801796d0a042d75ddcbb4
Tom Forbes <tom@tomforb.es>
2019-11-10 00:07:10 +0000

    Add a cd-project function

--- .config/fish/functions/open-project.fish
+++ .config/fish/functions/open-project.fish
@@ -1,8 +1,6 @@
-function open-project -d "Open a project"
-  set filter "$argv"
-  set chosen_project (git workspace list | fzf -q "$filter")
-  if string length -q -- $chosen_project
-     charm $GIT_WORKSPACE/$chosen_project
-     pushd $GIT_WORKSPACE/$chosen_project
+function open-project -d "Open a project in PyCharm"
+  set result cd-project $argv
+  if string length -q -- $result
+    charm $result
   end
 end
