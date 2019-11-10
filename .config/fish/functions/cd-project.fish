6e5a001bce310e1e237b621cc06292c920f3b1e8
Tom Forbes <tom@tomforb.es>
2019-11-10 00:08:32 +0000

    Fix functions

--- .config/fish/functions/cd-project.fish
+++ .config/fish/functions/cd-project.fish
@@ -3,6 +3,6 @@ function cd-project -d "Move to a project"
   set chosen_project (git workspace list | fzf -q "$filter")
   if string length -q -- $chosen_project
      pushd $GIT_WORKSPACE/$chosen_project
-     return $GIT_WORKSPACE/$chosen_project
+     echo $GIT_WORKSPACE/$chosen_project
   end
 end
