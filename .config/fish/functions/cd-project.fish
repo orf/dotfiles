862c51138c843a8bfa71199d1257a92cd966680f
Tom Forbes <tom@tomforb.es>
2021-12-26 13:37:22 +0000

    Begin switch to chezmoi

--- .config/fish/functions/cd-project.fish
+++
@@ -1,8 +0,0 @@
-function cd-project -d "Move to a project"
-  set filter "$argv"
-  set chosen_project (git workspace list | fzf -q "$filter")
-  if string length -q -- $chosen_project
-     pushd $GIT_WORKSPACE/$chosen_project
-     echo $GIT_WORKSPACE/$chosen_project
-  end
-end
--- .config/fish/functions/open-project.fish
+++
@@ -1,6 +0,0 @@
-function open-project -d "Open a project in PyCharm"
-  set result (cd-project $argv)
-  if string length -q -- $result
-    charm $result
-  end
-end
