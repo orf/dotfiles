2c18b33a3ea5e8a95fe9f2bfe641bfbc1789c8c9
Tom Forbes <tom@tomforb.es>
2023-04-17 12:29:57 +0100

    Update run_once_setup_rust.sh

--- run_once_setup_rust.sh
+++ run_once_setup_rust.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh --emulate sh
 set -uo pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- '-y'
 source "$HOME/.cargo/env"
 rustup toolchain install nightly
