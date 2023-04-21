48a4f615aedd3801597fb072bc01f2b1e6f60d52
Tom Forbes <tom@tomforb.es>
2023-04-21 15:33:27 +0100

    Disable keyboard shortcuts

--- run_once_setup_macos_app_preferences.sh
+++ run_once_setup_macos_app_preferences.sh
@@ -6,3 +6,15 @@ set -uo pipefail
 osascript -e 'quit app "Magnet"'
 defaults import com.crowdcafe.windowmagnet.xml "$(chezmoi source-path)/preferences/com.crowdcafe.windowmagnet.xml"
 osascript -e 'tell app "Magnet" to activate'
+
+
+VALUE="$(cat "$(chezmoi source-path)"/preferences/keyboard_shortcuts.xml)"
+
+defaults write pbs NSServicesStatus \
+  -dict-add \
+  'com.apple.Terminal - Open man Page in Terminal - openManPage' \
+  "$VALUE"
+defaults write pbs NSServicesStatus \
+  -dict-add \
+  'com.apple.Terminal - Search man Page Index in Terminal - searchManPages' \
+  "$VALUE"
