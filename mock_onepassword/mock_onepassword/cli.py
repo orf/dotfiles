20fd02778d1553b46b40413bc6838e865ea2f512
Tom Forbes <tom@tomforb.es>
2023-05-08 16:05:37 +0100

    Random output

--- mock_onepassword/mock_onepassword/cli.py
+++ mock_onepassword/mock_onepassword/cli.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-
-# {{ onepasswordRead "op://chezmoi/ssh/public key" }}
-# {{ onepasswordRead "op://chezmoi/github/password" | quote }}
-# set -gx GITLAB_TOKEN {{ onepasswordRead "op://chezmoi/gitlab/password" | quote }}
-# {{ onepasswordRead "op://chezmoi/ssh/private key" }}
-import random
+import secrets
 
 
 def main():
-    print("test")
+    print(secrets.token_urlsafe(12))
 
 
 if __name__ == '__main__':
