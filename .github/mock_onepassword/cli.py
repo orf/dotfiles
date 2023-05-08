757d6bc2928001448700e5517269db641abf2d21
Tom Forbes <tom@tomforb.es>
2023-05-08 15:44:06 +0100

    Try this

--- .github/mock_onepassword/cli.py
+++ .github/mock_onepassword/cli.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
 # {{ onepasswordRead "op://chezmoi/ssh/public key" }}
 # {{ onepasswordRead "op://chezmoi/github/password" | quote }}
 # set -gx GITLAB_TOKEN {{ onepasswordRead "op://chezmoi/gitlab/password" | quote }}
 # {{ onepasswordRead "op://chezmoi/ssh/private key" }}
+import random
+
 
 def main():
-    pass
+    print("test")
 
 
 if __name__ == '__main__':
