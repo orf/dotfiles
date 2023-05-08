5916fe7b9308b48bd8d74429f0d0088dc2646766
Tom Forbes <tom@tomforb.es>
2023-05-08 15:17:37 +0100

    oh-my-zsh setup

--- run_once_setup_1_zsh.sh
+++ run_once_setup_1_zsh.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export ZSH=~/.config/oh-my-zsh/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
