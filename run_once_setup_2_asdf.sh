85ddec043303b9ba41e701941ac0e9739fe206f2
Tom Forbes <tom@tomforb.es>
2023-05-12 17:02:18 +0100

    Don't uninstall

--- run_once_setup_2_asdf.sh
+++ run_once_setup_2_asdf.sh
@@ -3,6 +3,8 @@
 asdf plugin add python
 asdf plugin add nodejs
 
-asdf list all nodejs
+# Force stuff to be downloaded
+asdf list all nodejs > /dev/null
+asdf list all python > /dev/null
 
 asdf install
