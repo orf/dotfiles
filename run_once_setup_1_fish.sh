7341c135ab302b7b693b4259865f6d5d221631b6
Tom Forbes <tom@tomforb.es>
2023-05-12 12:04:56 +0100

    Disable the homebrew cache in favour of the cellar

---
+++ run_once_setup_1_fish.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+fish -c "/opt/homebrew/opt/fzf/install --all --xdg"
+fish -c "fisher update"
---
+++ run_once_setup_1_macos_shell.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+set -uo pipefail
+
+print "Adding /opt/homebrew/bin/fish to the launchctl path"
+sudo launchctl config user path "/opt/homebrew/bin:$PATH"
+
+if ! grep -Fxq "/opt/homebrew/bin/fish" /etc/shells; then
+  print "Fish not in /etc/shells, adding"
+  echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
+fi
+
+if [[ ! -v CI ]]; then
+  chsh -s /opt/homebrew/bin/fish
+fi
+
