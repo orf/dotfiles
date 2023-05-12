595a8960a893715b68d4fcadb51ec4941c2d6644
Tom Forbes <tom@tomforb.es>
2023-05-12 13:57:34 +0100

    Handle the case where firefoxdeveloperedition is not available as a HTTP handler

--- run_once_setup_1_utilities.sh
+++ run_once_setup_1_utilities.sh
@@ -7,5 +7,9 @@ ssh-keyscan github.com >>~/.ssh/known_hosts 2>&1
 ssh-keyscan gitlab.com >>~/.ssh/known_hosts 2>&1
 
 git lfs install
-defaultbrowser firefoxdeveloperedition
 
+if defaultbrowser | grep -q "firefoxdeveloperedition"; then
+   defaultbrowser firefoxdeveloperedition
+else
+   echo "Not setting defaultbrowser as firefoxdeveloperedition is not available"
+fi
