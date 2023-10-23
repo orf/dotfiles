9ffc3121c8998b93ea4411cd2fecea890beadbac
Tom Forbes <tom@tomforb.es>
2023-10-23 16:14:33 +0100

    Add fisher hash

--- run_once_setup_1_fish.sh
+++ run_once_setup_1_fish.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
+# fisher hash: {{ include (joinPath .chezmoi.sourceDir "dot_config" "fish" "fish_plugins" | quote) | sha256sum }}
+
 fish -c "$(brew --prefix fzf)/install --all --xdg"
 fish -c "fisher update"
