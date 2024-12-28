f4f55a67ffc6faec70f27086d60d5500cee37bd9
Thomas Forbes <tom.forbes@gitguardian.com>
2024-12-28 15:02:49 +0000

    Move cargo config

--- run_once_setup_2_mise.sh
+++ run_once_setup_2_mise.sh
@@ -3,4 +3,5 @@ set -uo pipefail
 
 # mise hash: {{ include (joinPath .chezmoi.sourceDir "dot_config" "mise" "config.toml" | quote) | sha256sum }}
 
+echo Hash: '{{ include (joinPath .chezmoi.sourceDir "dot_config" "mise" "config.toml" | quote) | sha256sum }}'
 mise install
