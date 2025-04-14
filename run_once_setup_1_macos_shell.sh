c85cc47cd3c1ebbc320cf756ab19b86411db6cd6
Tom Forbes <tom@tomforb.es>
2025-04-14 19:39:04 +0100

    Update run_once_setup_1_macos_shell.sh

--- run_once_setup_1_macos_shell.sh
+++ run_once_setup_1_macos_shell.sh
@@ -1,17 +1,17 @@
 #!/bin/zsh
 set -uo pipefail
 
-PREFIX=$(brew --prefix)
+# PREFIX=$(brew --prefix)
 
-print "Adding $PREFIX/bin/fish to the launchctl path"
-sudo launchctl config user path "$PREFIX/bin:$PATH"
+# print "Adding $PREFIX/bin/fish to the launchctl path"
+# sudo launchctl config user path "$PREFIX/bin:$PATH"
 
-if ! grep -Fxq "$PREFIX/bin/fish" /etc/shells; then
-  print "Fish not in /etc/shells, adding"
-  echo "$PREFIX/bin/fish" | sudo tee -a /etc/shells
-fi
+# if ! grep -Fxq "$PREFIX/bin/fish" /etc/shells; then
+#   print "Fish not in /etc/shells, adding"
+#   echo "$PREFIX/bin/fish" | sudo tee -a /etc/shells
+# fi
 
-if [[ ! -v CI ]]; then
-  chsh -s "$PREFIX"/bin/fish
-fi
+# if [[ ! -v CI ]]; then
+#   chsh -s "$PREFIX"/bin/fish
+# fi
 
