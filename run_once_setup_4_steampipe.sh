87924278e391ecc585b2002cbf33a5b19b0377ac
Tom Forbes <tom@tomforb.es>
2025-04-14 14:20:55 +0100

    Update run_once_setup_4_steampipe.sh

--- run_once_setup_4_steampipe.sh
+++ run_once_setup_4_steampipe.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 set -uo pipefail
 
-steampipe plugin install francois2metz/gitguardian
-steampipe plugin install aws
-steampipe plugin install github
+# steampipe plugin install francois2metz/gitguardian
+# steampipe plugin install aws
+# steampipe plugin install github
