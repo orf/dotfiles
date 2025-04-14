f8317b383c536f9a25ef5513f56b1489102d2f3c
Tom Forbes <tom@tomforb.es>
2025-04-14 19:39:37 +0100

    Update run_once_setup_1_utilities.sh

--- run_once_setup_1_utilities.sh
+++ run_once_setup_1_utilities.sh
@@ -8,20 +8,20 @@ ssh-keyscan gitlab.com >>~/.ssh/known_hosts 2>&1
 
 git lfs install
 
-if defaultbrowser | grep -q "firefoxdeveloperedition"; then
-   defaultbrowser firefoxdeveloperedition
-else
-   echo "Not setting defaultbrowser as firefoxdeveloperedition is not available"
-fi
+# if defaultbrowser | grep -q "firefoxdeveloperedition"; then
+#    defaultbrowser firefoxdeveloperedition
+# else
+#    echo "Not setting defaultbrowser as firefoxdeveloperedition is not available"
+# fi
 
-appDir="$HOME/Library/PreferencePanes/SwiftDefaultApps.prefPane/Contents/Resources/ThisAppDoesNothing.app"
-if [ -d "$appDir" ]; then
-  echo "$appDir exists, setting up"
-  # Remove quanrantine flag
-  xattr -d com.apple.quarantine "$appDir" || true
-  # Open the app
-  open "$appDir"
-  echo "Setup complete"
-else
-  echo "$appDir does not exist, not opening"
-fi
+# appDir="$HOME/Library/PreferencePanes/SwiftDefaultApps.prefPane/Contents/Resources/ThisAppDoesNothing.app"
+# if [ -d "$appDir" ]; then
+#   echo "$appDir exists, setting up"
+#   # Remove quanrantine flag
+#   xattr -d com.apple.quarantine "$appDir" || true
+#   # Open the app
+#   open "$appDir"
+#   echo "Setup complete"
+# else
+#   echo "$appDir does not exist, not opening"
+# fi
